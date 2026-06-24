ae33f8af0f59fdd0d10d6024986ec969d5e6203a
lihaihua <lihaihua@zhisuaninfo.com.cn>
2026-06-24 14:35:51 +0800

    fix: 修复自定义文件夹选择器插件
    - 将插件移到正确的包路径
    - 修复插件注册
    - 恢复文件选择器功能

--- android/app/src/main/java/com/musicplayer/local/FolderPickerPlugin.java
+++ android/app/src/main/java/com/musicplayer/local/FolderPickerPlugin.java
@@ -1,4 +1,4 @@
-package com.musicplayer.folderpicker;
+package com.musicplayer.local;
 
 import android.app.Activity;
 import android.content.Intent;
--- android/app/src/main/java/com/musicplayer/local/MainActivity.java
+++ android/app/src/main/java/com/musicplayer/local/MainActivity.java
@@ -2,7 +2,6 @@ package com.musicplayer.local;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.musicplayer.folderpicker.FolderPickerPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
